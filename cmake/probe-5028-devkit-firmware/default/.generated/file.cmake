8bb41df51e6a1b8410ad24c25fcfb8a7d680a009
joshua-cobian <joshua.cobian@copeland.com>
2025-11-12 11:53:30 -0500

    configured MCC

--- cmake/probe-5028-devkit-firmware/default/.generated/file.cmake
+++ cmake/probe-5028-devkit-firmware/default/.generated/file.cmake
@@ -7,7 +7,7 @@ foreach(source_file ${probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_a
         set_source_files_properties(${source_file} PROPERTIES INCLUDE_DIRECTORIES "$<PATH:NORMAL_PATH,$<PATH:REMOVE_FILENAME,${source_file}>>")
 endforeach()
 
-set(probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_assemblePreprocess)
+set(probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_assemblePreprocess "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/protected_io.S")
 set_source_files_properties(${probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_assemblePreprocess} PROPERTIES LANGUAGE ASM)
 
 # For assembly files, add "." to the include path for each file so that .include with a relative path works
@@ -15,7 +15,18 @@ foreach(source_file ${probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_a
         set_source_files_properties(${source_file} PROPERTIES INCLUDE_DIRECTORIES "$<PATH:NORMAL_PATH,$<PATH:REMOVE_FILENAME,${source_file}>>")
 endforeach()
 
-set(probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_compile "${CMAKE_CURRENT_SOURCE_DIR}/../../../main.c")
+set(probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_compile
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/main.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/adc/src/adc0.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/dac/src/dac0.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/nvm/src/nvm.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/clock.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/config_bits.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/interrupt.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/pins.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/system/src/system.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/timer/src/tca0.c"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../../../My_MCC_Config/mcc/mcc_generated_files/vref/src/vref.c")
 set_source_files_properties(${probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_compile} PROPERTIES LANGUAGE C)
 set(probe_5028_devkit_firmware_default_default_XC8_FILE_TYPE_link)
 set(probe_5028_devkit_firmware_default_image_name "default.elf")
